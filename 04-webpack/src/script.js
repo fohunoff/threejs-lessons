e82e88a302982d98885888e4666c2ea23512b289
Ivan Zagainov <fohunoff@gmail.com>
2024-03-21 11:56:04 +0300

    check transform properties

--- 04-webpack/src/script.js
+++ 04-webpack/src/script.js
@@ -2,6 +2,8 @@ import * as THREE from 'three';
 
 import './style.css';
 
+const canvas = document.querySelector('#webgl');
+
 // Sizes
 const sizes = {
   width: 800,
@@ -14,21 +16,29 @@ const scene = new THREE.Scene();
 // Red cube
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+mesh.position.set(0.7, -0.6, 1); // set position
+
+// Rotation
+mesh.rotation.reorder('YXZ');
+mesh.rotation.x = Math.PI * 0.25;
+mesh.rotation.y = Math.PI * 0.25;
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
-// camera.position.x = 2;
-// camera.position.y = 1;
 scene.add(camera);
 
+camera.lookAt(mesh.position); // camera look at Vector3 (or position object)
+
 // Renderer
-const canvas = document.querySelector('#webgl');
-const renderer = new THREE.WebGLRenderer({
-  canvas,
-});
+const renderer = new THREE.WebGLRenderer({ canvas });
 
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
