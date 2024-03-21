743d7796324faa1f43aabca312ef1fca0b4a237a
Ivan Zagainov <fohunoff@gmail.com>
2024-03-21 12:16:00 +0300

    add mesh in group

--- 04-webpack/src/script.js
+++ 04-webpack/src/script.js
@@ -13,18 +13,31 @@ const sizes = {
 // Scene
 const scene = new THREE.Scene();
 
-// Red cube
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+// Groups
+const group = new THREE.Group();
+scene.add(group);
 
-mesh.position.set(0.7, -0.6, 1); // set position
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
 
-// Rotation
-mesh.rotation.reorder('YXZ');
-mesh.rotation.x = Math.PI * 0.25;
-mesh.rotation.y = Math.PI * 0.25;
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+
+cube2.position.x = -2
+cube3.position.x = 2
+
+group.add(cube1);
+group.add(cube2);
+group.add(cube3);
 
 // Axes helper
 const axesHelper = new THREE.AxesHelper();
@@ -34,8 +47,6 @@ scene.add(axesHelper);
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
 scene.add(camera);
-
-camera.lookAt(mesh.position); // camera look at Vector3 (or position object)
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({ canvas });
