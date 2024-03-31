34faad530a01b4fb648a212102fcad3d47b9c538
Ivan Zagainov <fohunoff@gmail.com>
2024-03-31 12:25:25 +0300

    handle fullscreen

--- 07-cameras/src/script.js
+++ 07-cameras/src/script.js
@@ -28,6 +28,14 @@ window.addEventListener('resize', () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
+window.addEventListener('dblclick', () => {
+  if (!document.fullscreenElement) {
+    canvas.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+});
+
 /**
  * Cursor
  */
