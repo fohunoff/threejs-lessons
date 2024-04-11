9dce3056b3271d2b90ee47c8b7840c30cef70a6a
Ivan Zagainov <fohunoff@gmail.com>
2024-04-11 20:02:02 +0300

    lights lesson init

--- src/script.js
+++ src/script.js
@@ -1,92 +1,61 @@
 import './style.css'
 import * as THREE from 'three'
-
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
-import { TextGeometry } from 'three//examples/jsm/geometries/TextGeometry.js'
-
-import * as dat from 'dat.gui';
-
-const gui = new dat.GUI({ closed: false, width: 400 }); // can pass init panel parameters
+import * as dat from 'lil-gui'
 
 /**
  * Base
  */
+// Debug
+const gui = new dat.GUI()
+
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
 
-// Axes helper
-const axesHelper = new THREE.AxesHelper()
-// scene.add(axesHelper)
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+
 
 /**
- * Texture
+ * Objects
  */
+// Material
+const material = new THREE.MeshStandardMaterial()
+material.roughness = 0.4
 
-const textureLoader = new THREE.TextureLoader();
-const matcapTexture = textureLoader.load('/textures/matcaps/1.png');
+// Objects
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 32, 32),
+    material
+)
+sphere.position.x = - 1.5
 
-/**
- * Fonts
- */
-const fontLoader = new FontLoader();
-
-fontLoader.load(
-  '/fonts/helvetiker_regular.typeface.json',
-  (font) => {
-    const BEVEL_SIZE = 0.02;
-    const BEVEL_THICKNESS = 0.03;
-
-    const textGeometry = new TextGeometry(
-      'fohunoff',
-      {
-        font: font,
-        size: 0.5,
-        height: 0.2,
-        curveSegment: 2,
-        bevelEnabled: true,
-        bevelSize: BEVEL_SIZE,
-        bevelThickness: BEVEL_THICKNESS,
-        bevelOffset: 0,
-        bevelSegments: 2
-      }
-    )
-
-    textGeometry.center()
-
-    const material = new THREE.MeshMatcapMaterial({
-      matcap: matcapTexture
-    })
-
-    const text = new THREE.Mesh(textGeometry, material);
-
-    scene.add(text)
-
-    const donutGeometry = new THREE.TorusBufferGeometry(0.3, 0.2, 20, 45);
-
-    for (let i = 0; i < 100; i++) {
-      const donut = new THREE.Mesh(donutGeometry, material);
-
-      donut.position.x = (Math.random() - 0.5) * 10 ;
-      donut.position.y = (Math.random() - 0.5) * 10 ;
-      donut.position.z = (Math.random() - 0.5) * 10 ;
-
-      donut.rotation.x = Math.random() * Math.PI;
-      donut.rotation.y = Math.random() * Math.PI;
-
-      const scaleRandom = Math.random();
-      donut.scale.x = scaleRandom
-      donut.scale.y = scaleRandom
-      donut.scale.z = scaleRandom
-
-      scene.add(donut)
-    } 
-  }
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(0.75, 0.75, 0.75),
+    material
 )
 
+const torus = new THREE.Mesh(
+    new THREE.TorusGeometry(0.3, 0.2, 32, 64),
+    material
+)
+torus.position.x = 1.5
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(5, 5),
+    material
+)
+plane.rotation.x = - Math.PI * 0.5
+plane.position.y = - 0.65
+
+scene.add(sphere, cube, torus, plane)
 
 /**
  * Sizes
@@ -144,10 +113,13 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    // Array.from([sphere, plane, torus]).forEach(mesh => {
-    //   mesh.rotation.y = 0.1 * elapsedTime;
-    //   mesh.rotation.x = 0.15 * elapsedTime;
-    // })
+    sphere.rotation.y = 0.1 * elapsedTime
+    cube.rotation.y = 0.1 * elapsedTime
+    torus.rotation.y = 0.1 * elapsedTime
+
+    sphere.rotation.x = 0.15 * elapsedTime
+    cube.rotation.x = 0.15 * elapsedTime
+    torus.rotation.x = 0.15 * elapsedTime
 
     // Update controls
     controls.update()
