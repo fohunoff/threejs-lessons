ab1740c684711fe4f4d824cbf1b9fbe676cd5313
Ivan Zagainov <fohunoff@gmail.com>
2024-04-11 13:14:07 +0300

    add random donut

--- src/script.js
+++ src/script.js
@@ -20,7 +20,14 @@ const scene = new THREE.Scene()
 
 // Axes helper
 const axesHelper = new THREE.AxesHelper()
-scene.add(axesHelper)
+// scene.add(axesHelper)
+
+/**
+ * Texture
+ */
+
+const textureLoader = new THREE.TextureLoader();
+const matcapTexture = textureLoader.load('/textures/matcaps/1.png');
 
 /**
  * Fonts
@@ -34,7 +41,7 @@ fontLoader.load(
     const BEVEL_THICKNESS = 0.03;
 
     const textGeometry = new TextGeometry(
-      'Hello World',
+      'fohunoff',
       {
         font: font,
         size: 0.5,
@@ -48,23 +55,38 @@ fontLoader.load(
       }
     )
 
-    textGeometry.computeBoundingBox();
+    textGeometry.center()
 
-    textGeometry.translate(
-      - (textGeometry.boundingBox.max.x - BEVEL_SIZE) * 0.5,
-      - (textGeometry.boundingBox.max.y - BEVEL_SIZE) * 0.5,
-      - (textGeometry.boundingBox.max.z - BEVEL_THICKNESS) * 0.5,
-    )
-
-    const textMaterial = new THREE.MeshBasicMaterial({
-      wireframe: true
+    const material = new THREE.MeshMatcapMaterial({
+      matcap: matcapTexture
     })
 
-    const text = new THREE.Mesh(textGeometry, textMaterial);
+    const text = new THREE.Mesh(textGeometry, material);
 
     scene.add(text)
+
+    const donutGeometry = new THREE.TorusBufferGeometry(0.3, 0.2, 20, 45);
+
+    for (let i = 0; i < 100; i++) {
+      const donut = new THREE.Mesh(donutGeometry, material);
+
+      donut.position.x = (Math.random() - 0.5) * 10 ;
+      donut.position.y = (Math.random() - 0.5) * 10 ;
+      donut.position.z = (Math.random() - 0.5) * 10 ;
+
+      donut.rotation.x = Math.random() * Math.PI;
+      donut.rotation.y = Math.random() * Math.PI;
+
+      const scaleRandom = Math.random();
+      donut.scale.x = scaleRandom
+      donut.scale.y = scaleRandom
+      donut.scale.z = scaleRandom
+
+      scene.add(donut)
+    } 
   }
 )
+
 
 /**
  * Sizes
