d8934e3e66852d98132848153a83bbd5e62a3e63
Ivan Zagainov <fohunoff@gmail.com>
2024-04-12 14:58:53 +0300

    shadow init lessons

--- src/script.js
+++ src/script.js
@@ -1,9 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
-import * as dat from 'dat.gui'
-
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper'
+import * as dat from 'dat.gui'
 
 /**
  * Base
@@ -19,107 +17,45 @@ const scene = new THREE.Scene()
 
 /**
  * Lights
- * 
- * -- Less
- * AmbientLight
- * HemisphereLight
- * 
- * -- Medium
- * DirectionalLight
- * PointLight
- * 
- * -- High
- * RectAreaLight
- * SpotLight
- * 
  */
-// const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // ... or
-const ambientLight = new THREE.AmbientLight();
-ambientLight.color = new THREE.Color(0xffffff);
-ambientLight.intensity = 0.5;
-scene.add(ambientLight);
+// Ambient light
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
+scene.add(ambientLight)
 
-gui.add(ambientLight, 'intensity').min(0).max(1).step(0.01);
+// Directional light
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
+directionalLight.position.set(2, 2, - 1)
+gui.add(directionalLight, 'intensity').min(0).max(1).step(0.001)
+gui.add(directionalLight.position, 'x').min(- 5).max(5).step(0.001)
+gui.add(directionalLight.position, 'y').min(- 5).max(5).step(0.001)
+gui.add(directionalLight.position, 'z').min(- 5).max(5).step(0.001)
+scene.add(directionalLight)
 
-const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 1)
-scene.add(hemisphereLight);
-
-const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3);
-directionalLight.position.set(1, 0.25, 0)
-scene.add(directionalLight);
-
-const pointLight = new THREE.PointLight(0xff9000, 0.5); // (color, intensity, distance, decay)
-pointLight.distance = 3;
-pointLight.decay = 2;
-pointLight.position.set(1, -0.5, 1);
-scene.add(pointLight);
-
-// work only with MeshStandardMaterial and MeshPhysicalMaterial
-const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 2, 1, 1); // color, intensity, width, height
-rectAreaLight.position.set(-1.5, 0, 1.5);
-rectAreaLight.lookAt(new THREE.Vector3());
-scene.add(rectAreaLight);
-
-const spotlight = new THREE.SpotLight(0x78ff00, 0.5, 10, Math.PI * 0.1, 0.25, 1); // color, intensity, distance, angle, penumbra (blur circle edge), decay
-spotlight.position.set(0, 2, 3)
-scene.add(spotlight);
-
-// need add target if want to move spotlight
-spotlight.target.position.x = -0.75;
-scene.add(spotlight.target);
-
-// Helpers
-const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 0.2);
-scene.add(hemisphereLightHelper);
-
-const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.2);
-scene.add(directionalLightHelper);
-
-const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2);
-scene.add(pointLightHelper);
-
-const spotLightHelper = new THREE.SpotLightHelper(spotlight);
-scene.add(spotLightHelper);
-window.requestAnimationFrame(() => spotLightHelper.update()) // просто фикс бага для правильно отображения в пространстве
-
-const rectAreaLightHelper = new RectAreaLightHelper(rectAreaLight);
-scene.add(rectAreaLightHelper); // maybe will be need to update position
-
-
+/**
+ * Materials
+ */
+const material = new THREE.MeshStandardMaterial()
+material.roughness = 0.7
+gui.add(material, 'metalness').min(0).max(1).step(0.001)
+gui.add(material, 'roughness').min(0).max(1).step(0.001)
 
 /**
  * Objects
  */
-// Material
-const material = new THREE.MeshStandardMaterial()
-material.roughness = 0.4
-
-// Objects
 const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(0.5, 32, 32),
     material
 )
-sphere.position.x = - 1.5
-
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(0.75, 0.75, 0.75),
-    material
-)
-
-const torus = new THREE.Mesh(
-    new THREE.TorusGeometry(0.3, 0.2, 32, 64),
-    material
-)
-torus.position.x = 1.5
 
 const plane = new THREE.Mesh(
     new THREE.PlaneGeometry(5, 5),
     material
 )
 plane.rotation.x = - Math.PI * 0.5
-plane.position.y = - 0.65
+plane.position.y = - 0.5
 
-scene.add(sphere, cube, torus, plane)
+scene.add(sphere, plane)
 
 /**
  * Sizes
@@ -175,15 +111,6 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
-
-    // Update objects
-    sphere.rotation.y = 0.1 * elapsedTime
-    cube.rotation.y = 0.1 * elapsedTime
-    torus.rotation.y = 0.1 * elapsedTime
-
-    sphere.rotation.x = 0.15 * elapsedTime
-    cube.rotation.x = 0.15 * elapsedTime
-    torus.rotation.x = 0.15 * elapsedTime
 
     // Update controls
     controls.update()
